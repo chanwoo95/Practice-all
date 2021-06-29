2a9d4bd8e4ed7e95d41a28ef83a94370a6062519
chanwoo95 <cksdndbd95@gmail.com>
2021-06-29 21:09:51 +0900

    markup

---
+++ react-todo-list/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './header/header.jsx';
+
+function App() {
+  return <>
+    <Header/>
+  </>
+}
+
+
+
+export default App;
---
+++ react-todo-list/src/header/header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return ( 
+    <>
+        <h2>TODO</h2>
+        <input type="text" />
+        <button></button>
+    </>)
+}
+
+export default Header;
