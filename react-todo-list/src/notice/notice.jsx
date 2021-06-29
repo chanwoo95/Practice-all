e4716484cc298ad899b60ebce8e8e8bd1e72024d
chanwoo95 <cksdndbd95@gmail.com>
2021-06-30 01:10:54 +0900

    markup

---
+++ react-todo-list/src/notice/notice.jsx
@@ -0,0 +1,16 @@
+function Notice() {
+    return (
+    <>
+        <div className="list--top">
+            <span>고정된 목록</span>
+            <button></button>
+        </div>
+        <div className="list--container">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </>)
+}
+
+export default Notice;
