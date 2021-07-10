85a78441ce51a24d3dac44cf501aecdd6f659004
chanwoo95 <cksdndbd95@gmail.com>
2021-07-10 11:19:56 +0900

    react

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -1,10 +1,26 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
-import './App.css';
+const App = () => {
+  const [videos, setVideos] = useState([]);
 
-function App() {
-  return (
-    
-  );
-}
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&key=AIzaSyBpXbgoPSkptnIkOZJHPYWzn9L-yEUKuus",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
+};
 
 export default App;
