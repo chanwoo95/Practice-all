7a16c667afcf516cccbaf390ed1bd5832f071810
chanwoo95 <cksdndbd95@gmail.com>
2021-08-02 22:22:14 +0900

    login css

--- react-card-maker/src/components/footer/footer.jsx
+++ react-card-maker/src/components/footer/footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 function Footer() {
-  return <h2>Login Layout</h2>;
+  return <h2 className={styles.h2}>Login Layout</h2>;
 }
 
 export default Footer;
