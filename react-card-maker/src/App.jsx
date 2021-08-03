7bf4cc20d8c774951fe81fe4a76a42bcdea4951b
chanwoo95 <cksdndbd95@gmail.com>
2021-08-03 19:12:00 +0900

    login page

--- react-card-maker/src/App.jsx
+++ react-card-maker/src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.module.css";
 import Login from "./components/login/login";
 import styles from "./App.module.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Maker from "./components/maker/maker";
 
 function App({ authService }) {
   return (
-    <section className={styles.app}>
-      <Login authService={authService} />
-    </section>
+    <Router>
+      <Route exact path="/">
+        <section className={styles.app}>
+          <Login authService={authService} />
+        </section>
+      </Route>
+      <Route path="/maker">
+        <Maker />
+      </Route>
+    </Router>
   );
 }
 
---
+++ react-card-maker/src/components/maker/maker.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Maker() {
+  return <h2>Maker Page</h2>;
+}
+
+export default Maker;
