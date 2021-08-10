0fa3151ebf1b1c66b55cc621679dc88ea2b37a3d
chanwoo95 <cksdndbd95@gmail.com>
2021-08-10 19:24:28 +0900

    card edit form

--- react-card-maker/src/components/card_edit_form/card_edit_form.jsx
+++ react-card-maker/src/components/card_edit_form/card_edit_form.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import styles from "./card_edit_form.module.css";
 
 function CardEditForm({ card }) {
+  const { name, company, job, theme, email, message } = card;
   return (
     <form>
-      <input type="text" className={styles.name} />
-      <input type="text" className={styles.company} />
+      <input value={name} type="text" className={styles.name} />
+      <input value={company} type="text" className={styles.company} />
       <select name="colors" id="">
         <option value="dark">Dark</option>
         <option value="pink">Pink</option>
         <option value="colorful">Colorful</option>
       </select>
-      <input type="text" className={styles.job} />
-      <input type="text" className={styles.email} />
-      <input type="text" className={styles.message} />
+      <input value={job} type="text" className={styles.job} />
+      <input value={email} type="text" className={styles.email} />
+      <textarea
+        value={message}
+        className={styles.message}
+        name="text"
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
     </form>
   );
 }
