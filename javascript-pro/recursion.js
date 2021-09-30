4b34294453cef0d06f9c2a80c51d499219d09eb7
chanwoo95 <cksdndbd95@gmail.com>
2021-09-30 16:51:50 +0900

    recursion function

--- javascript-pro/recursion.js
+++ javascript-pro/recursion.js
@@ -11,3 +11,20 @@ function pow(x, n) {
 }
 
 pow(2, 3);
+
+// recursion function
+
+function pow(x, n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+pow(2, 4);
+
+//
+function pow(x, n) {
+  return n == 1 ? x : x * pow(x, n - 1);
+}
+pow(2, 5);
