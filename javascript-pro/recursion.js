af0d5174c26eb30759077adcfea75a0029cdec7f
chanwoo95 <cksdndbd95@gmail.com>
2021-10-01 20:25:33 +0900

    recursion

--- javascript-pro/recursion.js
+++ javascript-pro/recursion.js
@@ -28,3 +28,35 @@ function pow(x, n) {
   return n == 1 ? x : x * pow(x, n - 1);
 }
 pow(2, 5);
+
+//
+let company = {
+  sales: [
+    {
+      name: "john",
+      salary: 3000,
+    },
+    { name: "bob", salary: 2000 },
+  ],
+
+  department: {
+    sites: [
+      {
+        name: "mike",
+        salary: 4000,
+      },
+      { name: "brian", salary: 1000 },
+    ],
+    internals: [
+      {
+        name: "kim",
+        salary: 3000,
+      },
+      { name: "lee", salary: 2000 },
+    ],
+  },
+};
+
+function sumSalaries(department) {}
+
+sumSalaries(company);
