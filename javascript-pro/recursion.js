f2efb0b1123357127e967479d56ddb7716cfd443
chanwoo95 <cksdndbd95@gmail.com>
2021-10-02 16:37:40 +0900

    recursion sum

--- javascript-pro/recursion.js
+++ javascript-pro/recursion.js
@@ -57,6 +57,16 @@ let company = {
   },
 };
 
-function sumSalaries(department) {}
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    return department.reduce((prev, current) => prev + current.salary, 0);
+  } else {
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep);
+    }
+    return sum;
+  }
+}
 
 sumSalaries(company);
