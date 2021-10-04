aca7b7de4e1eac957c4ee57da3d405168ba5b1ff
chanwoo95 <cksdndbd95@gmail.com>
2021-10-04 20:19:30 +0900

    foreach basic

--- javascript-pro/foreach.js
+++ javascript-pro/foreach.js
@@ -1,5 +1,20 @@
 let arr = ["😈", "😋", "😥"];
 
-arr.forEach((item, index) => {
-  alert(`my emotion is ${item} and index ${index}`);
-});
+// arr.forEach((item, index) => {
+//   alert(`my emotion is ${item} and index ${index}`);
+// });
+
+arr.forEach((item) => console.log(item));
+
+// for version
+
+let test = ["1", "2,", "3", "4"];
+let copy = [];
+
+for (let i = 0; i < test.length; i++) {
+  copy.push(test[i]);
+}
+
+// foreach version
+
+test.forEach((item) => copy.push(item));
