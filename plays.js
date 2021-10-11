f5296d78552d1884fd841a129c8310d1eed2ea91
chanwoo95 <cksdndbd95@gmail.com>
2021-10-11 09:57:43 +0900

    json files

---
+++ plays.js
@@ -0,0 +1,5 @@
+export default {
+  hamlet: { name: "hamlet", type: "tragedy" },
+  aslike: { name: "as-like", type: "comedy" },
+  othello: { name: "othello", type: "tragedy" },
+};
---
+++ refactoring/invoices.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    customer: "BigCo",
+    performances: [
+      { playId: "hamlet", audience: 55 },
+      { playId: "as-like", audience: 35 },
+      { playId: "othello", audience: 40 },
+    ],
+  },
+];
