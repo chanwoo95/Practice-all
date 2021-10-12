d79ccdd6987f86472211f57c56c80551f14d82a3
chanwoo95 <cksdndbd95@gmail.com>
2021-10-12 17:45:54 +0900

    volume Credits

--- refactoring/invoices.js
+++ refactoring/invoices.js
@@ -3,7 +3,7 @@ export default [
     customer: "BigCo",
     performances: [
       { playId: "hamlet", audience: 55 },
-      { playId: "as-like", audience: 35 },
+      { playId: "aslike", audience: 35 },
       { playId: "othello", audience: 40 },
     ],
   },
---
+++ refactoring/plays.js
@@ -0,0 +1,5 @@
+export default {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  aslike: { name: "As You Like it", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
